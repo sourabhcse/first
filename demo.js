c76c52a98625854ca618d43c85e3568ae45d6a7e
sourabhcse <sourabhsaini201098@gmail.com>
2022-11-17 15:36:49 +0530

    addednew

--- demo.js
+++ demo.js
@@ -19,7 +19,12 @@ var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
 items[1].textContent='Raj';
+items[0].style.fontWeight='bold';
 items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='yellow';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+
+
 
 
