4dc5709eaa59da81a491bb1862f6fd6670877fd8
sourabhcse <sourabhsaini201098@gmail.com>
2022-11-18 08:16:59 +0530

    elementbychild

--- demo.js
+++ demo.js
@@ -37,7 +37,7 @@ items[2].style.backgroundColor='green';
 //li[3].style.fontWeight='bold';
 //li[2].style.backgroundColor='green';
 
-var sed=document.querySelector('.list-group-item:nth-child(2)');
+/*var sed=document.querySelector('.list-group-item:nth-child(2)');
 sed.style.backgroundColor = 'green';
 
 var td=document.querySelector('.list-group-item:nth-child(3)');
@@ -50,7 +50,45 @@ var odd=document.querySelectorAll('li:nth-child(odd)');
 for (var i=0; i<odd.length;i++)
 {
     odd[i].style.backgroundColor='red';
-}
+}*/
 
+var itemList=document.querySelector('#items');
+console.log(itemList.parentElement);
+itemList.parentElement.style.background='yellow'
+console.log(itemList.parentElement.parentElement.parentElement);
 
+//lastchild
+console.log(itemList.lastChild);
+// lastelementchild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent='vijay';
+
+//firstchild
+console.log(itemList.firstChild);
+// firstelementchild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='jay';
+
+//nextsibling
+console.log(itemList.nextSibling);
+// nextelement
+console.log(itemList.nextElementSibling);
+
+//previoussibling
+console.log(itemList.previousSibling);
+// previouselement
+console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='pink';
+
+//craeteelement
+var  newDiv=document.createElement('div');
+newDiv.className='ram';
+newDiv.id='kk';
+newDiv.setAttribute('title','jjjj');
+var newDivText=document.createTextNode('world');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+container.insertBefore(newDiv, h1)
 
