599dc6f846e6f6e0c71a328912cc80f42107755a
sourabhcse <sourabhsaini201098@gmail.com>
2022-11-30 12:38:46 +0530

    promise

---
+++ promise.js
@@ -0,0 +1,42 @@
+const posts=[
+    {title:'post one', body:'this is post one',craetedat:Date.now()},
+    {title:'post two', body:'this is post two',craetedat:Date.now()}
+];
+
+function getPosts(){
+    setTimeout(() => {
+        let output='';
+        posts.forEach((post,index)=>{
+            output+=`<li>${post.title} ${post.body} ${post.craetedat}</li>`;
+        });
+        document.body.innerHTML=output;
+        
+    }, 1000);
+}
+
+function createPost(post){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            posts.push(post);
+            const error=true;
+            if(!error){
+                resolve();
+            }else{
+                reject('Error:something went wrong');
+            }
+        }, 2000);
+    });
+
+    
+}
+//createPost({title:'post three', body:'this is post three'}).then(getPosts);
+
+const promise1=Promise.resolve('Hello World');
+const promise2=10;
+const promise3=new Promise((resolve,reject)=>
+    setTimeout(resolve,2000,'Goodbye')
+);
+const promise4= fetch('')
+
+Promise.all([promise1,promise2,promise3]).then(values=>console.log(values));
+
