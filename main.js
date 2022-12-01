ea2a2e06954e2e73aac59f50381e4826f7d5fe8c
sourabhcse <sourabhsaini201098@gmail.com>
2022-12-01 10:54:32 +0530

    appoint network

---
+++ main.js
@@ -0,0 +1,55 @@
+let sendBtn = document.getElementById('send');
+let resetBtn = document.getElementById('reset');
+let form = document.getElementById('form');
+
+//Form Refresh State
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+//Now Working For Reset Btn
+resetBtn.addEventListener('click', (e) => {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    //Set Value
+    name.value = '';
+    email.value = '';
+    message.value = '';
+
+});
+
+//Now Start SendBtn
+sendBtn.addEventListener('click', (e) => {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    //Set Value And LocalStorage
+
+    name = name.value;
+    //localStorage.setItem('name', name);
+
+    email = email.value;
+    //localStorage.setItem('email', email);
+
+    message = message.value;
+    //localStorage.setItem('message', message);
+
+    const obj={
+        name,
+        email,
+        message
+    }
+
+    axios.post("https://crudcrud.com/api/dfa0e737bdba4d6299ef9bcf7d56eca6/appoint",obj)
+        .then((Response)=>{console.log(Response)})
+        .catch((err)=>{
+            console.log(err);
+        })
+
+
+
+
+});
