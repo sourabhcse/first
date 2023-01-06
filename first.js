d2177d30e03253e36bf89558b1657c7fb9c54042
sourabhcse <sourabhsaini201098@gmail.com>
2023-01-06 23:29:39 +0530

    body parser

--- first.js
+++ first.js
@@ -2,21 +2,31 @@
 //const routes=require('./routes.js');
 
 const express = require('express');
+const bodyParser=require('body-parser');
 
 //console.log(routes.text);
 
+
 const app=express();
 
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next)=>{
     console.log("in the middleware");
-    next();
+    res.send('<form action ="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button>');
 
 });
 
-app.use((req,res,next)=>{
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/',(req,res,next)=>{
     console.log("in the another middleware");
     res.send(`<h1>Hello From Express</h1>`);
-    res.send({key1:"hello"});
 });
 //const server = http.createServer(app);
 
