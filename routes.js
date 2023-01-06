e1cc351aee6906bef86e9884a4fd18deab4cca5f
sourabhcse <sourabhsaini201098@gmail.com>
2023-01-06 14:57:38 +0530

    debug node

--- routes.js
+++ routes.js
@@ -28,7 +28,7 @@ else if (url === '/message' && method === 'POST') {
     });
     return req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split('=')[1];
+        const message = parsedBody.split('=')[0];
         fs.writeFileSync('message.text', message);
         res.statusCode = 302;
         res.setHeader('Location', '/');
